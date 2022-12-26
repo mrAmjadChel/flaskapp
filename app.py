404da7298f831b28a95132528994a80acb99d849
mrAmjadChel <amjadchelaeh.8@gmail.com>
2022-12-26 18:39:45 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, flash, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
+app.secret_key = 'hello'
+
+@app.route('/success/<name>')
+def success(name):
+   return render_template('myself.html')
+
+
+@app.route('/',methods = ['POST','GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      session['user'] = user
+      return redirect(url_for('user'))
+   else:
+      user = request.args.get('nm')
+      return render_template('login.html')
+
+@app.route('/user')
+def user():
+   if 'user' in session:
+      user = session['user']
+      return redirect(url_for('success',name = user))
+   else:
+      return redirect(url_for(''))
+
+@app.route('/logout')
+def logout():
+   session.pop('user', None)
+   flash('You were successfully logged out')
+   return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='5000',debug=True)
+    
